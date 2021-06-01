f21468099fc652336e8b1ae4508adcd2a4020fef
jared-pinfold <j.pinfold@gmail.com>
2021-06-01 16:40:23 +1200

    made progress on styling. Handing over to Sam to take over

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -36,36 +36,52 @@ function App (props) {
   return (
     <>
       <div className='app container'>
+
+        {/* header */}
         <div className='header'>
           <h1>Jared and Sam's Astonishing Randomizer of Humans</h1>
         </div>
 
-        {/* name input form */}
-        <form onSubmit={handleSubmit}>
-          <label>
-            Add a name:
-            <input type='text' name='name' onChange={handleChange} />
-          </label>
-          <input type='submit' value='Submit' />
-        </form>
-        <div className='list'>
-          <h1>Seed list of Humans</h1>
-          <button onClick={randomise}>Randomise</button>
-          <ul>
-            {humans.map(human => (
-              <><li key={human}>{human}</li><button className={human} onClick={handleDelete}>X</button></>
-            ))}
-          </ul>
-        </div>
+        {/* Inputs bar */}
+       
 
-        {/* Output randomised list */}
-        <div className='list'>
-          <h1>Random Humans</h1>
-          <ul>
-            {list.map(person => (
-              <li key={person}>{person}</li>
-            ))}
-          </ul>
+        {/* Lists */}
+        <div className='container'>
+
+          <div className='list'>
+          <div className='container'>
+          <form onSubmit={handleSubmit}>
+            <label>
+            Add a name:
+              <input type='text' name='name' onChange={handleChange} />
+            </label>
+            <input type='submit' value='Submit' />
+          </form>
+
+        </div>
+            <div>
+              <h1>Seed list of Humans</h1>
+              <ul>
+                {humans.map(human => (
+                  <><div key={human}><button className={human} onClick={handleDelete}>X</button>  {human}</div></>
+                ))}
+              </ul>
+            </div>
+
+            <div className='list'>
+            <button onClick={randomise}>Randomise</button>
+              <div>
+                <h1>Random Humans</h1>
+                <ul>
+                  {list.map(person => (
+                    <div className='match_button_height' key={person}>{person}</div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+          </div>
+
         </div>
       </div>
     </>
