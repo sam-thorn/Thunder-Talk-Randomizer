63a894786eecc4747434fda86d990d51313652db
jared-pinfold <j.pinfold@gmail.com>
2021-06-07 15:10:54 +1200

    removed some old commented out code

--- client/components/appHelper.js
+++ client/components/appHelper.js
@@ -1,25 +1,3 @@
-// export const humans = [
-//   'Jared',
-//   'Erin',
-//   'Johann',
-//   'Sam',
-//   'Yuan',
-//   'Rogan',
-//   'Rebecca',
-//   'Johnny',
-//   'Grace',
-//   'Sarah',
-//   'Kateti',
-//   'Jonathan',
-//   'Karol'
-// ]
-
-// function addPersonToList () {
-//   x = randomRange(0, (humans.length - 1)) //Randomise person
-//   randomisedList.push(humans[x]) //Add person to randomised list
-//   humans.splice(x,1) //Remove person from seed list
-// }
-
 function randomRange (myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
 }
@@ -34,17 +12,8 @@ export function createRandomList (arr) {
     rawArray.splice(x, 1) // Remove person from seed list
   }
 
-  // function shufflePersonToTop(name) {
-  //   const isName = (element) => element === name
-  //   const toDelete = randomisedList.findIndex(isName)
-  //   randomisedList.splice(toDelete, 1)
-  //   randomisedList.unshift(name)
-  // }
-
   while (rawArray.length > 0) {
     addPersonToList()
   }
-  // shufflePersonToTop('Sarah')
-  // shufflePersonToTop('Rebecca')
   return randomisedList
 }
